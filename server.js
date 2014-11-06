3691a5c9a533a1101101ea91ca0ea88bdc74db25
Way Spurr-Chen <wayspurrchen@gmail.com>
2014-11-05 21:12:57 -0600

    Update server with port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app     = express();
-var server  = app.listen(3000);
+var server  = app.listen(process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
